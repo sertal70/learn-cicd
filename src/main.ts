ff8dae1000cf8f7a5ba7dc6ea2bdc4d6bb6710f6
Sergio Talente <sergio@sergiotalente.com>
2024-06-12 18:39:50 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, TypeScript with Express!');
+});
+
+app.get('/echo/:value', (req, res) => {
+  res.send(`You sent: ${req.params.value}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
